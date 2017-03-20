fffa52dd5ab4843e7c96cba186891bff0252847a
Matt Bessey <mbessey@caring.com>
2017-03-19 21:58:17 -0700

    AVERAGE deploy process

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 
 mix edeliver build release --verbose
 mix edeliver deploy release to production --verbose
-mix edeliver start production --verbose
+mix edeliver restart production --verbose
 mix edeliver migrate production --verbose
